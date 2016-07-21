b5dd74f7d12c85849620ee5ae4541aa16586afe0
yamamori.akihiro <yamamori.akihiro@donuts.ne.jp>
2016-07-21 14:21:58 +0900

    Implemented csv output func

--- src/main/java/jp/ne/donuts/ServerBenchMark.java
+++ src/main/java/jp/ne/donuts/ServerBenchMark.java
@@ -15,17 +15,35 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 public class ServerBenchMark {
 
 
-    static int numTasks = 2000;
+    static final int numTasks = 1000;
+    static final String host = "104.155.235.219";
+    //static final String host = "localhost";
+    static final String port = "4000";
+
     static ArrayBlockingQueue<Long> execTimes = new ArrayBlockingQueue<>(numTasks);
     public static void main(String[] args) throws IllegalStateException, IOException, InterruptedException {
         List<Socket> sockets = IntStream.range(0,numTasks)
             .mapToObj(String::valueOf)
             .map(str -> createSocket(str)).collect(Collectors.toList());
+        /*
         for(Socket s: sockets){
-            long startTime = System.currentTimeMillis();
+            long startTime = System.nanoTime();
             connect(s).chan("rooms:lobby", JsonNodeFactory.instance.nullNode()).join()
                             .receive("ok", (msg) -> disconnect(s,startTime));
         }
+        */
+        sockets.parallelStream()
+            .map(socket -> {
+                try {
+                    long startTime = System.nanoTime();
+                    return connect(socket)
+                            .chan("rooms:lobby", JsonNodeFactory.instance.nullNode())
+                            .join()
+                            .receive("ok", (msg) -> disconnect(socket,startTime));
+                } catch (IllegalStateException | IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }).count();
         while(execTimes.size()<numTasks){
             Thread.sleep(1000L);
         }
@@ -35,23 +53,13 @@ public class ServerBenchMark {
             );
         }
         System.out.println(execTimes.stream().collect(Collectors.summarizingDouble(i->i)));
-        /*
-        sockets.map(socket -> connect(socket))
-            .map(socket -> {
-                try {
-                    return socket.chan("rooms:lobby", JsonNodeFactory.instance.nullNode()).join()
-                            .receive("ok", (msg) -> disconnect(socket));
-                } catch (IllegalStateException | IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }).count();
-        */ 
+        System.exit(0);
 
     }
     
     private static Socket createSocket(String userName){
         try {
-            return new Socket("ws://104.155.235.219:4000/socket/websocket?user_name="+userName);
+            return new Socket("ws://"+host+":"+port+"/socket/websocket?user_name="+userName);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -69,8 +77,8 @@ public class ServerBenchMark {
     private static Socket disconnect(Socket socket, long startTime){
         try {
             //System.out.println(System.currentTimeMillis()-startTime+"[ms]");
+            execTimes.put((System.nanoTime()-startTime)/1_000_000);
             socket.disconnect();
-            execTimes.put(System.currentTimeMillis()-startTime);
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
